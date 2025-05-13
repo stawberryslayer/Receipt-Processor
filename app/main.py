8b373fd6cb58e977fc9399e700982606adcbed26
stawberryslayer <ywang2776@wisc.edu>
2025-05-12 22:22:21 -0500

    Match 400 and 404 message to the documentation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,21 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uuid
 from app.processor import calculate_points
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.status import HTTP_400_BAD_REQUEST
 
 app = FastAPI()
 receipts = {}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=HTTP_400_BAD_REQUEST,
+        content={"detail": "The receipt is invalid."},
+    )
 
 class Item(BaseModel):
     shortDescription: str
@@ -24,8 +36,8 @@ def process_receipt(receipt: Receipt):
     receipts[receipt_id] = points
     return {"id": receipt_id}
 
-@app.get("/receipts/{receipt_id}/points")
-def get_points(receipt_id: str):
-    if receipt_id in receipts:
-        return {"points": receipts[receipt_id]}
-    raise HTTPException(status_code=404, detail="Receipt not found")
+@app.get("/receipts/{id}/points")
+def get_points(id: str):
+    if id in receipts:
+        return {"points": receipts[id]}
+    raise HTTPException(status_code=404, detail="No receipt found for that ID.")
